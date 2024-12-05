cfbd4199a739ac153b2d4391e0c876c088772502
mausanabria <mau_sanabria@hotmail.com>
2024-12-05 17:20:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
- // Array con preguntas de ejemplo
+
+const nav = document.querySelector(".tabs-container");
+
+const offset = nav.offsetTop;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= offset) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        const offset = 100; // Ajusta según tus necesidades
+        const top = target.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top, behavior: 'smooth' });
+      }
+    });
+  });
+  
+
+  document.querySelectorAll('.dropdown-button').forEach(button => {
+    button.addEventListener('click', function () {
+        const dropdown = this.parentElement;
+
+        // Cierra otros menús abiertos
+        document.querySelectorAll('.dropdown').forEach(d => {
+            if (d !== dropdown) {
+                d.classList.remove('open');
+            }
+        });
+
+        // Alterna la visibilidad del menú actual
+        dropdown.classList.toggle('open');
+    });
+});
+
+// Cierra el menú si se hace clic fuera de él
+window.addEventListener('click', function (e) {
+    if (!e.target.closest('.dropdown')) {
+        document.querySelectorAll('.dropdown').forEach(d => d.classList.remove('open'));
+    }
+});
+
+
+
+
+// Array con preguntas de ejemplo
 const preguntas = [
     "Significado de la palabra Taekwon-do",
     "Nombre del fundador",
@@ -30,7 +83,7 @@ const preguntas = [
     "Diferencias entre Arte Marcial y Deporte",
     "Historia del Taekwon-Do Argentino",
     "Secciones del Cuerpo",
-    "Significado y Cantidad de Movimientos del Choong-Moo",
+    "Significado y Cantidad de Movimientos de Choong-Moo",
     "¿Qué Representa el Color Negro del Cinturón?",
 ];
 
@@ -38,7 +91,7 @@ const urls = {
     "Diferencias entre Arte Marcial y Deporte":"rojoptanegra.html#diferencias-entre-Arte-marcial-y-deporte",
     "Historia del Taekwon-Do Argentino":"rojoptanegra.html#historia-del-taekwon-Do-argentino",
     "Secciones del Cuerpo":"rojoptanegra.html#secciones-del-cuerpo",
-    "Significado y Cantidad de Movimientos del Choong-Moo":"rojoptanegra.html#significado-y-cantidad-de-movimientos-del-choong-moo",
+    "Significado y Cantidad de Movimientos de Choong-Moo":"rojoptanegra.html#significado-y-cantidad-de-movimientos-de-choong-moo",
     "¿Qué Representa el Color Negro del Cinturón?":"rojoptanegra.html#¿Que-Representa-el-Color-Negro-del-Cinturon?",
 }
 
@@ -82,3 +135,4 @@ document.addEventListener("click", (e) => {
         suggestionsBox.style.display = "none";
     }
 });
+
